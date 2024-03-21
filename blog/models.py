4c17fa152b40aff8cd8c3e2214f18bbb93196d18
marianachoratto <marianachoratto@gmail.com>
2024-03-21 18:42:36 -0300

    fazendo as models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model): 
+    titulo= models.CharField(blank=False, max_length=100, null=False )
+    autor= models.CharField(blank=False, max_length=70, null=False ) 
+    date= models.DateTimeField(auto_now_add=True)
+    preview= models.TextChoices(blank=False, max_length=200, null=False)
+    content= models.TextChoices(blank=False, null=False)
+    slug= models.SlugField()
+
+    class Meta:
+        ordering= ['-date']
+
+class Comentario(models.Model):
+    usuario = models.CharField(max_length=50, blank=True)
+    comentario = models.TextField(blank=False, max_length=200)
+    data2= models.DateField(auto_now_add=True)
