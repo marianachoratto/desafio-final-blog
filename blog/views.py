8407aa2ad4f9cbfda233570c0fb034d8f53a0b2f
marianachoratto <marianachoratto@gmail.com>
2024-03-22 11:41:45 -0300

    feat: fazendo o views e o index.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# View da página inicial 
+def index_html(request):
+    return render(request, 'index.html')
