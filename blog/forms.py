705ea0e336e0f80a234b631ca3ab79634613527e
marianachoratto <marianachoratto@gmail.com>
2024-03-28 12:55:59 -0300

    feat: tentando linkar os comentários com o banco de dados

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,28 @@
 from django import forms
-# from blog.models import Comentario
+from blog.models import Comentario
 
-class ComentarioForm(forms.Form):
-    # class Meta: 
-    #     model = Comentario
-    #     fields= ['usuario', 'comentario']
-    nome_login = forms.CharField(
-        label = "Nome de Login",
+class ComentarioForm(forms.ModelForm):    
+    usuario = forms.CharField(
+        label = "Nome do usuário:",
         required = True,
-        max_length = 100
+        max_length = 100,
+        widget= forms.TextInput(
+            attrs={
+                'class':'form-control'
+            }
+        )
+    )
+    comentario = forms.CharField(
+        label = "Comentário:",
+        required = True,
+        max_length = 500,
+        widget= forms.TextInput(
+            attrs={
+                'class':'form-control mb-3' 
+            }
+        )
     )
     
+    class Meta: 
+        model = Comentario
+        fields = ['usuario', 'comentario']
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
 from blog.forms import ComentarioForm
+# from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import redirect
+
 
 # View da página inicial 
 def index_html(request):
@@ -10,15 +14,41 @@ def index_html(request):
 
 
 def resenha_do_livro(request):
+    sucesso = False
     posts = Post.objects.get(pk=2) #aqui tbm é uma lista de objetos, que traz só 1 item
+    print("request", request)
     form = ComentarioForm()
     # print({'posts': posts})
+    print("sucesso", sucesso)
     context = {
         'posts': posts,
-        'form': form
+        'form': form,
+        "sucesso": sucesso
     }
+    print(context)
     return render(request, 'post1.html', context)
 
-# def comentario(request):
-#     form = ComentarioForm()
-#     return render(request, 'post1.html', {'form': form} )
+# @api_view(['POST'])
+def fazer_comentario(request):
+    if request.method =='POST':
+        form = ComentarioForm(request.POST)
+        print("form", form)
+        if form.is_valid():
+            form.save()
+            sucesso = True
+            print("posts", posts)
+
+            dados = {
+                'form': form,
+                'posts': posts,
+                "sucesso": sucesso
+            }
+            # return render(request, 'post1.html', {'dados': dados })
+            # return redirect("/crepusculo", {'dados': dados })
+            sucesso = True
+            return redirect("/crepusculo")
+        print(form.is_valid())
+    else: 
+        form = ComentarioForm()
+    return render(request, 'post1.html', {'form': form})
+
